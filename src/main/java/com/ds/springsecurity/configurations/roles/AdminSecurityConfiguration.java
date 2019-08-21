3cdaf359a73b9c5d2b84b3952a3f39985cf56e37
Divanio Silva <dcsilva@mail.ru>
2019-08-21 14:06:16 +0100

    Backup

--- src/main/java/com/ds/springsecurity/configurations/roles/AdminSecurityConfiguration.java
+++ src/main/java/com/ds/springsecurity/configurations/roles/AdminSecurityConfiguration.java
@@ -1,4 +1,60 @@
-package com.ds.springsecurity.configurations;
+package com.ds.springsecurity.configurations.roles;
 
-public class AdminSecurityConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.DigestAuthenticationFilter;
+
+@Configuration
+@Order(1)
+public class AdminSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/admin").addFilter(getDigestAuthFilter()).exceptionHandling()
+                .authenticationEntryPoint(getDigestEntryPoint())
+                .and().authorizeRequests().antMatchers("/test")
+                .hasRole("ADMIN");
+    }
+
+    public DigestAuthenticationFilter getDigestAuthFilter() throws Exception {
+        DigestAuthenticationFilter digestFilter = new DigestAuthenticationFilter();
+        digestFilter.setUserDetailsService(userDetailsServiceBean());
+        digestFilter.setAuthenticationEntryPoint(getDigestEntryPoint());
+        digestFilter.setPasswordAlreadyEncoded(true);
+        digestFilter.setCreateAuthenticatedToken(true);
+        return digestFilter;
+    }
+
+    private DigestAuthenticationEntryPoint getDigestEntryPoint(){
+        DigestAuthenticationEntryPoint entryPoint = new DigestAuthenticationEntryPoint();
+        entryPoint.setRealmName("admin-digest-realm");
+        entryPoint.setKey("fjkf33DD312_+");
+        return entryPoint;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password1").roles("USER").and()
+                .withUser("admin").password("18d94adb8db016d4aed2502f88ca6e56").roles("ADMIN");
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
--- src/main/java/com/ds/springsecurity/controllers/Controller.java
+++ src/main/java/com/ds/springsecurity/controllers/Controller.java
@@ -1,4 +1,19 @@
 package com.ds.springsecurity.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+    @GetMapping(value = "/admin")
+    public String admin(){
+        return "admin is logged in";
+    }
+
+    @GetMapping(value = "/user")
+    public String user(){
+        return "user is logged in";
+    }
+
 }
